831e26f49dad974922c97745dc7e314dd2741f40
S.Shrijayanth <81725639+Shrijayanth-Suresh@users.noreply.github.com>
2022-04-10 12:08:14 +0530

    Add files via upload

---
+++ Jmaster.java
@@ -0,0 +1,58 @@
+// For ur Kind Info, Remove comments to execute Specific Blocks of Command...
+/*  
+Day 1
+Github Intro
+/*
+Day 2
+Java Intro
+/*
+Day 3
+Java Implementation
+*//*
+public class Jmaster
+{
+    public static void main(String args[])
+{
+    int a=1;
+    int b=2, c=3, d=4;
+    float e=17645.156475f;
+    long f=10000000000l;
+    double g=2233445566d;
+    System.out.println(g);
+}
+}
+*/
+/*
+// Day4 
+public class Jmaster
+{
+    public static void main(String args[])
+{
+    int h=1__________3;
+    char a=66; // to determine ascii value
+    char b='6'; 
+    char c='A';
+    boolean d=true;
+    boolean e=(1==0);
+    String f= "Shrijayanth";
+    String l= "S";
+    String fl=f+' '+l;
+    String g= "\"Shri is doin\" \n java class\\training, under abi\t";
+    String u= "To Infinity and Beyond".toUpperCase();
+    String j= "To Infinity and Beyond".toLowerCase();
+    String k= "1,2,3,4,5"; // In output mention .length method for execution
+    int i= "12345".length();
+    if(e)
+    {
+        System.out.println("true");
+    }
+    System.out.println(k.length());
+}
+}
+*/
+// Day 5
+public class Jmaster
+{
+    public static void main(String args[])
+
+
